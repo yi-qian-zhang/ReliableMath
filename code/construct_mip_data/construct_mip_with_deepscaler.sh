c54bcf4096487d06ded0474cf93b801bd308b957
Zhang Yiqian <15065170282@163.com>
2025-11-19 06:48:20 +0800

    update 11-18

--- code/construct_mip_data/construct_mip_with_deepscaler.sh
+++ code/construct_mip_data/construct_mip_with_deepscaler.sh
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=7 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Distill
     --gpu_memory_utilization 0.9 \
     --port 8715
 
-CUDA_VISIBLE_DEVICES=055 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
+CUDA_VISIBLE_DEVICES=0,5 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
     --served-model-name DeepSeek-R1-Distill-Qwen-7B \
     --max-model-len 8192 \
     --tensor_parallel_size 2 \
@@ -129,9 +129,9 @@ CUDA_VISIBLE_DEVICES=7 vllm /data1/HF-Models/deepseek-ai/DeepSeek-R1-Distill-Qwe
     --port 8715
 
 #启动DeepSeek-R1-Distill-Qwen-7B 双卡
-CUDA_VISIBLE_DEVICES=1,2 vllm serve /data1/HF-Models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
+CUDA_VISIBLE_DEVICES=0,1 vllm serve /data1/HF-Models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
     --served-model-name DeepSeek-R1-Distill-Qwen-7B \
-    --max-model-len 8192 \
+    --max-model-len 16384 \
     --tensor_parallel_size 2 \
     --gpu_memory_utilization 0.9 \
     --port 8715
--- code/construct_mip_data/construct_mip_with_deepscaler_num.sh
+++ code/construct_mip_data/construct_mip_with_deepscaler_num.sh
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=7 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Distill
 
 CUDA_VISIBLE_DEVICES=4,5 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
     --served-model-name DeepSeek-R1-Distill-Qwen-7B \
-    --max-model-len 8192 \
+    --max-model-len 16384 \
     --tensor_parallel_size 2 \
     --gpu_memory_utilization 0.9 \
     --port 8717
@@ -25,7 +25,7 @@ CUDA_VISIBLE_DEVICES=4,5 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Disti
 # DeepSeek-R1-Distill-Qwen-7B 双卡
 CUDA_VISIBLE_DEVICES=6,7 vllm serve /shared/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
     --served-model-name DeepSeek-R1-Distill-Qwen-7B \
-    --max-model-len 8192 \
+    --max-model-len 16384 \
     --tensor_parallel_size 2 \
     --gpu_memory_utilization 0.9 \
     --port 8715
