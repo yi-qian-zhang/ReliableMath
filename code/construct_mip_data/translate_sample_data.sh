7fee021ec0a82102a5658409a5e968246e057fb3
Zhang Yiqian <15065170282@163.com>
2025-11-19 14:51:54 +0800

    update files

---
+++ code/construct_mip_data/translate_sample_data.sh
@@ -0,0 +1,6 @@
+python code/construct_mip_data/translate_sample_data.py \
+  --input data/DeepSeek-R1-Distill-Qwen-32B/11-19/official_mode/missing_three/19-19/polaris_easy_20_sample_valid_n3.json \
+  --output data/DeepSeek-R1-Distill-Qwen-32B/11-19/official_mode/missing_three/19-19/polaris_easy_20_sample_valid_n3_zh.json \
+  --model_url http://localhost:8719/v1 \
+  --model_name DeepSeek-R1-Distill-Qwen-32B  \
+  --threads 16
