81fdbcaf208a553a51a46f4b20732c434fa5dfb9
Zhang Yiqian <15065170282@163.com>
2025-11-21 06:31:16 +0800

    Merge remote changes

---
+++ code/extract_verification_results.sh
@@ -0,0 +1,3 @@
+python code/extract_verification_results.py \
+  data/DeepSeek-R1-Distill-Qwen-32B/11-20/official_mode/missing_one/data/solve/polaris_hard_40/polaris_hard_40_final_n1.json \
+  -o data/DeepSeek-R1-Distill-Qwen-32B/11-20/official_mode/missing_one/data/solve/polaris_hard_40/analysis_results.json
